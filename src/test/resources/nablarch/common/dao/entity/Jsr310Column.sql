bef4a394d39bf07eb39a0efd0843606be2b5c0e8
oguchi-shiori03 <oguchi.shiori@tis.co.jp>
2024-03-06 19:38:51 +0900

    feat: DBのDate、Timestampの精度の差異によるデータ修正

--- src/test/resources/nablarch/common/dao/entity/Jsr310Column.sql
+++ src/test/resources/nablarch/common/dao/entity/Jsr310Column.sql
@@ -1,9 +1,9 @@
-find_where_local_date = 
+find_where_local_date_greater_than =
 select *
 from jsr310_column
-where local_date = ?
+where local_date > ?
 
-find_where_local_date_time =
+find_where_local_date_time_greater_than =
 select *
 from jsr310_column
-where local_date_time = ?
+where local_date_time > ?
